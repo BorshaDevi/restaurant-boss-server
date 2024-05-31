fb03d05786ce8fb511f6de86a1ec9bd310feb484
Borshadevi <borshadevi27@gmail.com>
2024-05-31 21:04:05 +0600

    add delete operation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express =require('express')
 const app=express()
+const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const cors=require('cors')
 const port=process.env.PORT || 5000
 
 //middle
+
 app.use(cors())
 app.use(express.json())
+
 
 
 
@@ -35,7 +39,55 @@ async function run() {
     const menuCollection = client.db("MenuDB").collection("menu");
     const reviewCollection=client.db("MenuDB").collection("reviews");
     const cartCollection=client.db("MenuDB").collection("carts");
-    app.get('/users',async(req,res)=>{
+
+
+    const verifyToken=(req,res,next)=>{
+      // console.log('verify site',req.headers.authorization)
+      if(!req.headers.authorization){
+           return res.status(401).send({message:'unauthorized'})
+      }
+      const token=req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.DE_ACCSED, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message:'unauthorized'})
+        }
+        
+        req.decoded=decoded
+       
+        next()
+      });
+      
+    }
+   
+    const verifyAdmin=async(req,res,next)=>{
+      const email=req.decoded.email
+      const query={email:email}
+      const user=await userCollection.findOne(query)
+      const isAdmin=user?.role === 'admin'
+      if(!isAdmin){
+        return res.status(403).send({message : 'forbidden access'})
+      }
+      
+      next()
+    }
+
+
+   app.get('/use/admin/:email',verifyToken,async(req,res)=>{
+         
+           const email=req.params.email
+           if(email !== req.decoded.email){
+            
+            return res.status(403).send({message:' forbidden access'})
+           }
+           const query={email:email}
+           const user=await userCollection.findOne(query)
+           let isAdmin=false
+           if(user){
+            isAdmin= user?.role ==='admin'
+           }
+           res.send({isAdmin})
+   })
+    app.get('/users',verifyToken,verifyAdmin,async(req,res)=>{
       const result=await userCollection.find().toArray()
       res.send(result)
     })
@@ -57,6 +109,17 @@ async function run() {
       const result=await reviewCollection.find().toArray()
       res.send(result)
     })
+    app.post('/menu',verifyToken,verifyAdmin,async(req,res)=>{
+      const menu=req.body
+      const result=await menuCollection.insertOne(menu)
+      res.send(result)
+    })
+    app.post('/jwt',async(req,res)=>{
+      const userInfo=req.body
+     const token= jwt.sign(userInfo, process.env.DE_ACCSED, { expiresIn: '1h' });
+    //  console.log(token)
+     res.send({token})
+    })
     app.post('/carts',async(req,res)=>{
       const cart=req.body
       const result=await cartCollection.insertOne(cart)
@@ -74,7 +137,7 @@ async function run() {
       const result=await userCollection.insertOne(user)
       res.send(result)
     })
-    app.patch('/userAdmin/:Id',async(req,res)=>{
+    app.patch('/userAdmin/:Id',verifyToken,async(req,res)=>{
        const id=req.params.Id
        const filter = { _id: new ObjectId(id) };
        const updateDoc = {
@@ -85,13 +148,20 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc)
       res.send(result)
     })
+    app.delete('/menuDelete/:id',verifyToken,verifyAdmin,async(req,res)=>{
+      const Id=req.params.id
+      const query={_id :new ObjectId (Id)}
+      const result=await menuCollection.deleteOne(query)
+      console.log(result)
+      res.send(result)
+    })
     app.delete('/delete/:id',async(req,res)=>{
       const Id=req.params.id
       const query={_id : new ObjectId(Id)}
       const result=await cartCollection.deleteOne(query)
       res.send(result)
     })
-    app.delete('/deleteUser/:id',async(req,res)=>{
+    app.delete('/deleteUser/:id',verifyToken,verifyAdmin,async(req,res)=>{
       const id=req.params.id
       const query={_id:new ObjectId(id)}
       const result=await userCollection.deleteOne(query)
